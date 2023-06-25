b0e99e77b1bb767185d18339d181a8d34e2c4d99
Calvin <calvin.lin@hotmail.com>
2023-06-25 00:36:40 -0700

    girds set

--- script/parallax.js
+++ script/parallax.js
@@ -1,7 +1,7 @@
-window.addEventListener('scroll',function(){
-  const header = document.querySelector('header');
-  header.classList.toggle("sticky", window.scrollY > 0);
-});
+// window.addEventListener('scroll',function(){
+//   const header = document.querySelector('header');
+//   header.classList.toggle("sticky", window.scrollY > 0);
+// });
 
 function togglemenu(){
   const menutoggle = document.querySelector('.menutoggle');
